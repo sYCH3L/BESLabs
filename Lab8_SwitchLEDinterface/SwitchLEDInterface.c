358c5fb7bc8ba7124551c2041fdb91eedc980737
Enari Saar <frontxx@gmail.com>
2017-10-05 02:44:37 +0300

    Lab8
    Lab8

---
+++ Lab8_SwitchLEDinterface/SwitchLEDInterface.c
@@ -0,0 +1,73 @@
+// ***** 0. Documentation Section *****
+// SwitchLEDInterface.c for Lab 8
+// Runs on LM4F120/TM4C123
+// Use simple programming structures in C to toggle an LED
+// while a button is pressed and turn the LED on when the
+// button is released.  This lab requires external hardware
+// to be wired to the LaunchPad using the prototyping board.
+// January 15, 2016
+//      Jon Valvano and Ramesh Yerraballi
+
+// ***** 1. Pre-processor Directives Section *****
+#include "TExaS.h"
+#include "tm4c123gh6pm.h"
+
+#define PE0                    (*((volatile unsigned long *)0x40024004))
+#define PE1                    (*((volatile unsigned long *)0x40024008))
+
+// ***** 2. Global Declarations Section *****
+
+// FUNCTION PROTOTYPES: Each subroutine defined
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void PortE_init(void);
+void Delay(unsigned long time);
+void toggleLed(void);
+// ***** 3. Subroutines Section *****
+
+// PE0, PB0, or PA2 connected to positive logic momentary switch using 10k ohm pull down resistor
+// PE1, PB1, or PA3 connected to positive logic LED through 470 ohm current limiting resistor
+// To avoid damaging your hardware, ensure that your circuits match the schematic
+// shown in Lab8_artist.sch (PCB Artist schematic file) or 
+// Lab8_artist.pdf (compatible with many various readers like Adobe Acrobat).
+int main(void){ 
+//**********************************************************************
+// The following version tests input on PE0 and output on PE1
+//**********************************************************************
+  TExaS_Init(SW_PIN_PE0, LED_PIN_PE1, ScopeOn);  // activate grader and set system clock to 80 MHz
+	PortE_init();
+	
+  EnableInterrupts();           // enable interrupts for the grader
+  while(1){
+		PE1 = 0x02;
+		Delay(1);
+		if(PE0 == 0x01)
+		{
+				PE1 ^= 0x02;
+				Delay(1); 
+		}
+  }
+  
+}
+void PortE_init(void)
+{
+	volatile unsigned long int delay = 0;
+	SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOE;
+	delay = SYSCTL_RCGC2_R;
+	
+	GPIO_PORTE_DIR_R |= 0x02;
+	GPIO_PORTE_DEN_R = 0x03;
+	GPIO_PORTE_PUR_R &= 0x0;
+	
+	
+}
+void Delay(unsigned long time){
+  unsigned long i;
+  while(time > 0){
+    i = 1333333;  // this number means 100ms
+    while(i > 0){
+      i = i - 1;
+    }
+    time = time - 1; // decrements every 100 ms
+  }
+}
